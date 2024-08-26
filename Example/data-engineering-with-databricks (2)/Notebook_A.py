acd62aa48ace0f04c9797d50fe9320cbfa775e31
govorovski <govorovski>
2024-08-26 15:46:14 +0000

    Init create

---
+++ Example/data-engineering-with-databricks (2)/Notebook_A.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+name = "John"
+print(f"Hello {name}")
+
+%cd /Workspace/Users/dmitri_govorovski@epam.com/data-engineering-with-databricks/
+%run ./Notebook-B
+
+print(f"Welcome back {full_name}")
